8d505e32e949ab67ef24eaeda133b0c8ca7cd1e4
Andres Hernandez <ahernandez@growthaccelerationpartners.com>
2018-06-13 23:52:26 -0600

    Disable FB login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,7 +21,7 @@ Devise.setup do |config|
     prompt: 'select_account',
     skip_jwt: true
   }
-  config.omniauth :facebook, FB_APP_ID, FB_APP_SECRET
+  # config.omniauth :facebook, FB_APP_ID, FB_APP_SECRET
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
